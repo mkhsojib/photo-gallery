b95c1f1ae3fc01f8647549e44ad52ee836bb5cd7
Md Kamrul hasan <mkhsojib@gmail.com>
2018-04-02 00:12:50 +0600

    Form Components & Views

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Photoshow</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/css/foundation.css">
+</head>
+<body>
+
+<div class="row">
+
+
+    @yield('content')
+
+</div>
+
+</body>
+</html>
