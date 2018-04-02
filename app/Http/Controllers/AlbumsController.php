f7f32328008b9e85af2e24e1c2976ec819ae3036
Md Kamrul hasan <mkhsojib@gmail.com>
2018-04-02 12:00:04 +0600

    create and display albums

--- app/Http/Controllers/AlbumsController.php
+++ app/Http/Controllers/AlbumsController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Album;
 
 class AlbumsController extends Controller
 {
     public function index()
     {
-        return view('albums.index');
+        $albums = Album::with('Photos')->get();
+
+
+        return view('albums.index')->with('albums', $albums);
 
     }
 
@@ -48,7 +52,19 @@ class AlbumsController extends Controller
 
         $path = $request->file('cover_image')->storeAs('public/album_covers', $filenameToStore);
 
-        return $path;
+        // create album
+
+
+        $album = new Album;
+
+        $album->name = $request->input('name');
+        $album->description = $request->input('description');
+        $album->cover_image = $filenameToStore;
+
+        $album->save();
+
+        return redirect('/albums')->with('success', 'Alubm Created');
+
 
     }
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,19 +10,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/css/foundation.css">
 </head>
 <body>
-
 @include('inc.topbar')
-
 <br>
-
 <div class="row">
-
     @include('inc.messages')
-
-
     @yield('content')
-
 </div>
-
 </body>
 </html>
